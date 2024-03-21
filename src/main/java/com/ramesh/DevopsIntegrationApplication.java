c4ecd88e4faaa438d624f41b839d0d9331b3e842
Ramesh <patel.ramesh541@gmail.com>
2024-03-21 17:29:44 +0530

    update

--- src/main/java/com/ramesh/DevopsIntegrationApplication.java
+++ src/main/java/com/ramesh/DevopsIntegrationApplication.java
@@ -3,7 +3,6 @@ package com.ramesh;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
-
 @SpringBootApplication
 @RestController
 public class DevopsIntegrationApplication {
