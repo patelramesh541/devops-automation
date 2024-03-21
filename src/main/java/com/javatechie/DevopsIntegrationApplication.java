d5f79704247e73d0ad000ef083745a23d7ed7c01
Ramesh <patel.ramesh541@gmail.com>
2024-03-21 14:28:17 +0530

    update

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to javatechie";
+		return "welcome ramesh ";
 	}
 
 	public static void main(String[] args) {
